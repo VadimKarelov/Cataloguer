75aeda0144933357c5b3833bfd30c6d4d1987ef1
VadimKarelov <vadim.karelov.02@mail.ru>
2023-09-23 20:30:41 +0500

    #3 Добавил интерфейс репозитория

---
+++ Cataloguer.Database/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Cataloguer.Database.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public T Get(Guid guid);
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
