b4c6d20225357755362de5c7ef30e659828b6a9f
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-04 18:24:22 +0500

    add init of some tables

--- Cataloguer.Database/Repositories/Context/CataloguerApplicationContext.cs
+++ Cataloguer.Database/Repositories/Context/CataloguerApplicationContext.cs
@@ -22,5 +22,63 @@ namespace Cataloguer.Database.Repositories.Context
         {
             Database.EnsureCreated();
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // не стоит менять порядок инициализации
+
+            modelBuilder.Entity<Town>().HasData(ReadTownsFromFile());
+
+            modelBuilder.Entity<Good>().HasData(DoWithNotification(ReadGoodsFromFile, "Чтение списка продуктов"));
+
+            modelBuilder.Entity<AgeGroup>().HasData(new AgeGroup[]
+                {
+                    new AgeGroup() { Description = "Дети", MinimalAge = 0, MaximalAge = 14 },
+                    new AgeGroup() { Description = "Подростки", MinimalAge = 15, MaximalAge = 20 },
+                    new AgeGroup() { Description = "Молодые", MinimalAge = 21, MaximalAge = 35 },
+                    new AgeGroup() { Description = "Взрослые", MinimalAge = 36, MaximalAge = 50 },
+                    new AgeGroup() { Description = "Пожилые", MinimalAge = 51, MaximalAge = short.MaxValue },
+                });
+
+            modelBuilder.Entity<Gender>().HasData(new Gender[]
+                {
+                    new Gender() { Name = "Мужской" },
+                    new Gender() { Name = "Женский" },
+                    new Gender() { Name = "Боевой вертолет" }
+                });
+        }
+
+        private Town[] ReadTownsFromFile()
+        {
+            Random random = new Random();
+
+            using StreamReader reader = new StreamReader("..\\..\\..\\Resources\\goroda.txt");
+            return reader.ReadToEnd()
+                .Split()
+                .Where(x => !string.IsNullOrEmpty(x) && !x.Contains("Оспаривается"))
+                .Select(x => new Town() { Name = x, Population = random.Next(5000, 20000000) })
+                .ToArray();
+        }
+
+        private Good[] ReadGoodsFromFile()
+        {
+            Random random = new Random();
+
+            using StreamReader reader = new StreamReader("..\\..\\..\\Resources\\goods.txt");
+            return reader.ReadToEnd()
+                .Split()
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Distinct()
+                .Select(x => new Good() { Name = x })
+                .ToArray();
+        }
+
+        private T DoWithNotification<T>(Func<T> func, string funcName)
+        {
+            Console.WriteLine($"Начало выполнения метода: {funcName}");
+            var res = func();
+            Console.WriteLine($"Начало выполнения метода: {funcName}");
+            return res;
+        }
     }
 }
