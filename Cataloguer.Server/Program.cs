a0b4f572e5ac451fcbde3abfb0dcb86711f7bc96
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-11 21:07:46 +0500

    add policy to allow acces from frontend

--- Cataloguer.Server/Program.cs
+++ Cataloguer.Server/Program.cs
@@ -9,6 +9,17 @@ namespace Cataloguer.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
