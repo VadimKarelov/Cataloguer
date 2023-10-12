7b24f2bfbfc4f721e6a463411148b1da45e03bca
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-12 21:41:01 +0500

    add command for getting single object

---
+++ Cataloguer.Database/Commands/GetCommands/GetCommand.cs
@@ -0,0 +1,27 @@
+﻿using Cataloguer.Database.Commands.Base;
+using Cataloguer.Database.Models;
+
+namespace Cataloguer.Database.Commands.GetCommands
+{
+    public class GetCommand<T> : AbstractCommand where T : class
+    {
+        public T? GetValueBuId(int id)
+        {
+            var typename = typeof(T).ToString();
+
+            switch (typename.Remove(0, typename.LastIndexOf(".") + 1))
+            {
+                case nameof(AgeGroup): return (Context.AgeGroups.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Brochure): return (Context.Brochures.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(BrochurePosition): return (Context.BrochurePositions.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Distribution): return (Context.Distributions.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Gender): return (Context.Genders.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Good): return (Context.Goods.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(SellHistory): return (Context.SellHistory.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Status): return (Context.Statuses.FirstOrDefault(x => x.Id == id) as T);
+                case nameof(Town): return (Context.Towns.FirstOrDefault(x => x.Id == id) as T);
+                default: throw new ArgumentException($"Указанного типа ({typename}) не существует в базе данных!");
+            }
+        }
+    }
+}
