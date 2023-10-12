6832864027e0ee7c8256ecdf90672f45f93b4691
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-12 21:03:37 +0500

    fix errors

--- Cataloguer.Database/Commands/GetCommands/GetListCommand.cs
+++ Cataloguer.Database/Commands/GetCommands/GetListCommand.cs
@@ -11,15 +11,15 @@ namespace Cataloguer.Database.Commands.GetCommands
 
             switch (typename.Remove(0, typename.LastIndexOf(".") + 1))
             {
-                case nameof(AgeGroup): return Context.AgeGroups.Cast<T>();
-                case nameof(Brochure): return Context.Brochures.Cast<T>();
-                case nameof(BrochurePosition): return Context.BrochurePositions.Cast<T>();
-                case nameof(Distribution): return Context.Distributions.Cast<T>();
-                case nameof(Gender): return Context.Genders.Cast<T>();
-                case nameof(Good): return Context.Goods.Cast<T>();
-                case nameof(SellHistory): return Context.SellHistory.Cast<T>();
-                case nameof(Status): return Context.Statuses.Cast<T>();
-                case nameof(Town): return Context.Towns.Cast<T>();
+                case nameof(AgeGroup): return Context.AgeGroups.Cast<T>().ToArray();
+                case nameof(Brochure): return Context.Brochures.Cast<T>().ToArray();
+                case nameof(BrochurePosition): return Context.BrochurePositions.Cast<T>().ToArray();
+                case nameof(Distribution): return Context.Distributions.Cast<T>().ToArray();
+                case nameof(Gender): return Context.Genders.Cast<T>().ToArray();
+                case nameof(Good): return Context.Goods.Cast<T>().ToArray();
+                case nameof(SellHistory): return Context.SellHistory.Cast<T>().ToArray();
+                case nameof(Status): return Context.Statuses.Cast<T>().ToArray();
+                case nameof(Town): return Context.Towns.Cast<T>().ToArray();
                 default: throw new ArgumentException($"Указанного типа ({typename}) не существует в базе данных!");
             }
         }
@@ -30,15 +30,15 @@ namespace Cataloguer.Database.Commands.GetCommands
 
             switch (typename.Remove(0, typename.LastIndexOf(".") + 1))
             {
-                case nameof(AgeGroup): return Context.AgeGroups.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Brochure): return Context.Brochures.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(BrochurePosition): return Context.BrochurePositions.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Distribution): return Context.Distributions.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Gender): return Context.Genders.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Good): return Context.Goods.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(SellHistory): return Context.SellHistory.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Status): return Context.Statuses.Where(x => predicate(x as T)).Cast<T>();
-                case nameof(Town): return Context.Towns.Where(x => predicate(x as T)).Cast<T>();
+                case nameof(AgeGroup): return Context.AgeGroups.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Brochure): return Context.Brochures.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(BrochurePosition): return Context.BrochurePositions.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Distribution): return Context.Distributions.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Gender): return Context.Genders.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Good): return Context.Goods.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(SellHistory): return Context.SellHistory.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Status): return Context.Statuses.Where(x => predicate(x as T)).Cast<T>().ToArray();
+                case nameof(Town): return Context.Towns.Where(x => predicate(x as T)).Cast<T>().ToArray();
                 default: throw new ArgumentException($"Указанного типа ({typename}) не существует в базе данных!");
             }
         }
