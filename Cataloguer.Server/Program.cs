eb5854abe8f3411a150f5076e557588040b0598b
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-12 20:54:22 +0500

    #12 Добавил запросы для получения всех видов сущностей

--- Cataloguer.Server/Program.cs
+++ Cataloguer.Server/Program.cs
@@ -8,6 +8,8 @@ namespace Cataloguer.Server
     {
         private const string _baseRoute = "/api/v1/cataloguer";
 
+        private static List<(string Route, string Description)> _existingRoutes = new();
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -26,20 +28,37 @@ namespace Cataloguer.Server
 
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", ShowAllRegisteredRoutes);
 
-            BrochureHandlerRegistration(app);
+            ListWithoutParametersRegistration(app);
 
             app.Run();
         }
 
-        private static void BrochureHandlerRegistration(WebApplication app)
+        private static void AddRoute(WebApplication app, string route, string description, Func<object> action)
         {
-            app.MapGet($"{_baseRoute}/getBrochures", 
-                () => JsonSerializer.Serialize(new GetListCommand<Brochure>().GetValues()));
+            app.MapGet($"{_baseRoute}/{route}", () => JsonSerializer.Serialize(action()));
 
-            app.MapGet($"{_baseRoute}/getAgeGroups",
-                () => JsonSerializer.Serialize(new GetListCommand<AgeGroup>().GetValues()));
+            _existingRoutes.Add(new($"{_baseRoute}/{route}", description));
+        }
+
+        private static string ShowAllRegisteredRoutes()
+        {
+            return string.Join('\n', _existingRoutes.Select(x => $"{x.Route} = {x.Description}"));
+        }
+
+        private static void ListWithoutParametersRegistration(WebApplication app)
+        {
+            // в идеале прописать пути: get/AgeGroup или get/Town, то есть указать сущность. но сойдет и так
+            AddRoute(app, "getAgeGroups", "no params", () => new GetListCommand<AgeGroup>().GetValues());
+            AddRoute(app, "getBrochures", "no params", () => new GetListCommand<Brochure>().GetValues());
+            AddRoute(app, "getBrochurePositions", "no params", () => new GetListCommand<BrochurePosition>().GetValues());
+            AddRoute(app, "getDistributions", "no params", () => new GetListCommand<Distribution>().GetValues());
+            AddRoute(app, "getGenders", "no params", () => new GetListCommand<Gender>().GetValues());
+            AddRoute(app, "getGoods", "no params", () => new GetListCommand<Good>().GetValues());
+            AddRoute(app, "getSellHistory", "no params", () => new GetListCommand<SellHistory>().GetValues());
+            AddRoute(app, "getStatuses", "no params", () => new GetListCommand<Status>().GetValues());
+            AddRoute(app, "getTowns", "no params", () => new GetListCommand<Town>().GetValues());
         }
     }
 }
