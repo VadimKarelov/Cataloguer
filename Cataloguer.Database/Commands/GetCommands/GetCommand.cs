942c01db314aa00a2e2a50f3c1cd1667ddca0bba
VadimKarelov <vadim.karelov.02@mail.ru>
2023-10-27 20:38:55 +0500

    Переделал Get запросы

--- Cataloguer.Database/Commands/GetCommands/GetCommand.cs
+++ Cataloguer.Database/Commands/GetCommands/GetCommand.cs
@@ -5,27 +5,180 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Cataloguer.Database.Commands.GetCommands
 {
-    public class GetCommand<T> : AbstractCommand where T : class
+    public class GetCommand : AbstractCommand
     {
         public GetCommand(DataBaseConfiguration configuration) : base(configuration) { }
 
-        public T? GetValueById(int id)
+        public AgeGroup? GetAgeGroup(int id)
         {
-            var typename = typeof(T).ToString();
+            return Context.AgeGroups
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
 
-            switch (typename.Remove(0, typename.LastIndexOf(".") + 1))
+        public IEnumerable<AgeGroup> GetListAgeGroup(Func<AgeGroup, bool>? predicate = null)
+        {
+            return TryApplyPredicate(Context.AgeGroups.AsNoTracking(), predicate).ToArray();
+        }
+
+        public Brochure? GetBrochure(int id, bool includeFields = false)
+        {
+            return includeFields ?
+                Context.Brochures
+                    .AsNoTracking()
+                    .Include(x => x.Status)
+                    .FirstOrDefault(x => x.Id == id) :
+                Context.Brochures
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Brochure> GetListBrochure(Func<Brochure, bool>? predicate, bool includeFields = false)
+        {
+            var request = Context.Brochures.AsNoTracking();
+
+            if (includeFields)
             {
-                case nameof(AgeGroup): return (Context.AgeGroups.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Brochure): return (Context.Brochures.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(BrochurePosition): return (Context.BrochurePositions.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Distribution): return (Context.Distributions.FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Gender): return (Context.Genders.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Good): return (Context.Goods.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(SellHistory): return (Context.SellHistory.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Status): return (Context.Statuses.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                case nameof(Town): return (Context.Towns.AsNoTracking().FirstOrDefault(x => x.Id == id) as T);
-                default: throw new ArgumentException($"Указанного типа ({typename}) не существует в базе данных!");
+                request.Include(x => x.Status);
             }
+
+            return TryApplyPredicate(request, predicate);
+        }
+
+        public BrochurePosition? GetBrochurePosition(int id, bool includeFields = false)
+        {
+            return includeFields ?
+                Context.BrochurePositions
+                    .AsNoTracking()
+                    .Include(x => x.Brochure)
+                    .Include(x => x.Good)
+                    .FirstOrDefault(x => x.Id == id) :
+                Context.BrochurePositions
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<BrochurePosition> GetListBrochurePositions(Func<BrochurePosition, bool>? predicate, bool includeFields = false)
+        {
+            var request = Context.BrochurePositions.AsNoTracking();
+
+            if (includeFields)
+            {
+                request.Include(x => x.Brochure)
+                    .Include(x => x.Good);
+            }
+
+            return TryApplyPredicate(request, predicate);
+        }
+
+        public Distribution? GetDistribution(int id, bool includeFields = false)
+        {
+            return includeFields ?
+                Context.Distributions
+                    .AsNoTracking()
+                    .Include(x => x.Town)
+                    .Include(x => x.AgeGroup)
+                    .Include(x => x.Brochure)
+                    .Include(x => x.Gender)
+                    .FirstOrDefault(x => x.Id == id) :
+                Context.Distributions
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Distribution> GetListDistribution(Func<Distribution, bool>? predicate, bool includeFields = false)
+        {
+            var request = Context.Distributions.AsNoTracking();
+
+            if (includeFields)
+            {
+                request.Include(x => x.Town)
+                    .Include(x => x.AgeGroup)
+                    .Include(x => x.Brochure)
+                    .Include(x => x.Gender);
+            }
+
+            return TryApplyPredicate(request, predicate);
+        }
+
+        public Gender? GetGender(int id)
+        {
+            return Context.Genders
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Gender> GetListGender(Func<Gender, bool>? predicate = null)
+        {
+            return TryApplyPredicate(Context.Genders.AsNoTracking(), predicate).ToArray();
+        }
+
+        public Good? GetGood(int id)
+        {
+            return Context.Goods
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Good> GetListGood(Func<Good, bool>? predicate = null)
+        {
+            return TryApplyPredicate(Context.Goods.AsNoTracking(), predicate).ToArray();
+        }
+
+        public SellHistory? GetSellHistory(int id, bool includeFields = false)
+        {
+            return includeFields ?
+                Context.SellHistory
+                    .AsNoTracking()
+                    .Include(x => x.Town)
+                    .Include(x => x.Good)
+                    .Include(x => x.Gender)
+                    .FirstOrDefault(x => x.Id == id) :
+                Context.SellHistory
+                    .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<SellHistory> GetListSellHistory(Func<SellHistory, bool>? predicate, bool includeFields = false)
+        {
+            var request = Context.SellHistory.AsNoTracking();
+
+            if (includeFields)
+            {
+                request.Include(x => x.Town)
+                    .Include(x => x.Good)
+                    .Include(x => x.Gender);
+            }
+
+            return TryApplyPredicate(request, predicate);
+        }
+
+        public Status? GetStatus(int id)
+        {
+            return Context.Statuses
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Status> GetListStatus(Func<Status, bool>? predicate = null)
+        {
+            return TryApplyPredicate(Context.Statuses.AsNoTracking(), predicate).ToArray();
+        }
+
+        public Town? GetTown(int id)
+        {
+            return Context.Towns
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Town> GetListTown(Func<Town, bool>? predicate = null)
+        {
+            return TryApplyPredicate(Context.Towns.AsNoTracking(), predicate).ToArray();
+        }
+
+        private static IEnumerable<T> TryApplyPredicate<T>(IQueryable<T> request, Func<T, bool>? predicate)
+        {
+            return predicate is null ? request.AsEnumerable() : request.Where(predicate);
         }
     }
 }
