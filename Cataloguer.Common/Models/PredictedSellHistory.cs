fd6b58e08f99656ac303b9000f2f8a78b3dda722
VadimKarelov <vadim.karelov.02@mail.ru>
2023-11-26 20:06:53 +0500

    Добавил новую модель для хранения предсказанных значений

---
+++ Cataloguer.Common/Models/PredictedSellHistory.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cataloguer.Common.Models;
+
+/// <summary>
+/// Хранит информацию о предсказанных значениях по продажам по каталогу
+/// </summary>
+[Table("predicted_sell_history")]
+public class PredictedSellHistory
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("brochure_id")]
+    public int BrochureId { get; set; }
+    
+    public Brochure? Brochure { get; set; }
+    
+    [Column("prediction_date")]
+    public DateTime PredictionDate { get; set; }
+    
+    /// <summary>
+    /// Предсказанное значение
+    /// </summary>
+    [Column("value")]
+    public decimal Value { get; set; }
+}
