7a3527452e6b1285f2efc17ca4ffe0517ace17d0
tedeev <tedeev@ursalab.ru>
2023-11-30 22:55:22 +0500

    Доработки в backend.
    Создал папку Scripts внутри Cataloguer.Server и поместил туда shell скрипты;
    Удалил лишнее из Cataloguer.Server.csproj;
    Удалил статические файлы из wwwroot.
    
    //Могу предложить Вадиму либо комментить сценарии для сборки, чтобы множественный перезапуск приложения не занимал значительное время;
    //либо сделать активными сценарии, связанные со сборкой, только если выполняется prod сборка.

--- frontend-app/src/components/App.tsx
+++ frontend-app/src/components/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
         <Layout className={"white-background-style"}>
             <Header className={"main-window-header"}>
                 <Typography.Title className={"header-title"}>
-                    Новый тетс эффективного каталога товаров
+                    Формирование эффективного каталога товаров
                 </Typography.Title>
             </Header>
             <Layout className={"main-window-layout"}>
